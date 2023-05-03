8f8a06c8b4970048de9dc9afb5c7787908a0ce6d
ZXHannnnnn <131329190+ZXHannnnnn@users.noreply.github.com>
2023-05-03 21:55:22 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,3 @@
+input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+})
